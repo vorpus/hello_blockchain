31c99defa6ed6b66c38ee4c9ed35b696b6db757f
vorpus <li3zhang@gmail.com>
2018-09-04 22:53:52 -0400

    some basic blockchain things

--- src/crypto.js
+++ src/crypto.js
@@ -3,3 +3,54 @@ import { sha256 } from 'js-sha256';
 export function hash(input) {
   return sha256(input);
 }
+
+export function calcD(totient, e) {
+  let a = e;
+  let b = 1;
+  let c = totient;
+  let d = totient;
+  let timesDivide;
+  let x;
+  let y;
+
+  while (a !== 1) {
+    timesDivide = Math.floor(c / a);
+    x = c - timesDivide * a;
+    y = d - timesDivide * b;
+
+    c = a;
+    d = b;
+    a = x;
+    b = y;
+    if ( b < 0 ) b += totient;
+  }
+  return b;
+}
+
+export function inverse(x, m) {
+  let a = 0;
+  let b = m;
+  let u = 1;
+  let q;
+  while (x>0) {
+    let xtemp = x;
+    let atemp = a;
+    q = Math.floor(b/x);
+    x = b % x;
+    a = u;
+    b = xtemp;
+    u = atemp - q * u;
+  }
+  if (b === 1) return a % m;
+  console.log('wtf');
+}
+
+export function dumbD(totient, e) {
+  let d = 0;
+  let answer;
+  while ( answer !== 1) {
+    d++;
+    answer = (e * d) % totient;
+  }
+  return d;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
-import { hash } from './crypto'
+import { hash, calcD, dumbD, inverse } from './crypto'
 
-document.body.innerHTML = hash('hello world');
+recalculate();
 
+document
+  .addEventListener('keyup', () => recalculate());
+
+document.getElementById('hash-button-1')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    calculateNonce(1);
+  });
+
+function calculateNonce(blockNumber) {
+  const hashedElement = document.getElementById('hashed-' + blockNumber);
+  const nonce = document.getElementById('nonce-' + blockNumber);
+  let hashedValue;
+  for (let i = 0; i < 500000; i++) {
+    hashedValue = hash(getText(blockNumber, i));
+    if (hashedValue.startsWith('0000')) {
+      nonce.value = i;
+      hashedElement.value = hashedValue;
+      break;
+    };
+  }
+}
+
+function recalculate() {
+  for (let i = 1; i <= 1; i++) {
+    computeHash(i);
+  }
+}
+
+function computeHash(blockNumber) {
+  const hashedElement = document.getElementById('hashed-' + blockNumber);
+  hashedElement.value = hash(getText(blockNumber));
+}
+
+function getText(blockNumber, nonce) {
+  nonce = nonce || document.getElementById('nonce-' + blockNumber).value;
+  const data = document.getElementById('data-' + blockNumber).value;
+  return hash(`${blockNumber}${nonce}${data}`);
+}
+
+window.dumbD = dumbD
+// console.warn(calcD(40, 7))
+// console.warn(dumbD(40, 7))
+//
+// console.warn(calcD(78*112, 2621))
+// console.warn(dumbD(78*112, 2621))
+console.warn(inverse(3120, 17))
